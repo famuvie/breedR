ab5d0739e1e865a9d5347ac66e2389cde2b96ff0
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2014-03-13 17:44:20 +0100

    tests for animal models and complex models (TODO)

---
+++ inst/tests/test-interactions.R
@@ -0,0 +1,5 @@
+#### Context: Models with several effects working together ####
+context("Models with several effects working together") 
+
+# TODO:
+# mixed effects models * spatial models * genetic effects
