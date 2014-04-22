ceaede737bac9b25b875273e27d7e44a3395523f
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2014-04-22 13:11:23 +0200

    fix colour in perspective plot variogram. Allow user coordinates.

--- R/variogram.R
+++ R/variogram.R
@@ -1,25 +1,63 @@
+# TODO
+# - number of observations per variogram cell 
+# - restrict R to cells where N > 30
 
 #' Empirical variograms of residuals
 #' 
 #' Computes isotropic and anisotropic empirical variograms from the residuals of
 #' a breedR model.
 #' 
-#' This assumes that there is at most one observation per spatial location. 
-#' Otherwise, are observations measured at different times? should a
+#' An empirical variogram computes the mean squared differences between 
+#' observations separated by a vector \code{h}, for all possible \code{h}. An 
+#' \code{isotropic} variogram assumes that the underlying process depends only 
+#' on the (euclidean) \emph{distance} between points, but not on the orientation
+#' or direction of \code{h}. At the other end, an \code{anisotropic} variogram 
+#' assumes that the process might depend on the orientation, but not on the 
+#' direction. Finally, \code{heat} and \code{perspective} are different 
+#' representations of a variogram which assumes that the process depends only on
+#' the absolute distance between rows and columns.
+#' 
+#' Unless \code{coord} or \code{z} are specified by the user, \code{variogram}
+#' plots
+#' 
+#' This function assumes that there is at most one observation per spatial 
+#' location. Otherwise, are observations measured at different times? should a 
 #' spatial-temporal variogram be fitted?
 #' 
+#' @param x a \code{breedR} result.
+#' @param plot character. What type of variogram is to be plotted. The default 
+#'   is 'all'. Other options are 'isotropic', 'heat', 'perspective' and 
+#'   'anisotropic'. See Details.
+#' @param R numeric. Radius of the variogram
+#' @param coord a two-column matrix with coordinates of observations
+#' @param z a numeric vector of values to be represented spatially
+#'   
+#'   
 #' @importFrom fields vgram.matrix
 #' @export
-variogram <- function(x, R, plot = c('all', 'isotropic', 'anisotropic', 'perspective', 'heat', 'none')) {
+variogram <- function(x, plot = c('all', 'isotropic', 'anisotropic', 'perspective', 'heat', 'none'), R, coord, z) {
   
-  if( !inherits(x, 'breedR') ) 
-    stop('This function works on models fitted with breedR')
+  if( !inherits(x, 'breedR') & (missing(coord) | missing(z)) ) 
+    stop(paste('This function works on models fitted with breedR,',
+               'or with both arguments coord and z.\n',
+               sep='\n'))
   
   plot <- match.arg(plot)
   # We need to place the residuals in matrix form
   # where rows and columns have the same spacing
-  coord <- coordinates(x)   # Coordinates in distance units (e.g. m)
-  resid <- residuals(x)
+  
+  # The user can provide coordinates for non-spatial remlf90 models
+  # Otherwise, use coordinates of fitted dataset
+  if(missing(coord)) {
+    # Coordinates in distance units (e.g. m)
+    coord <- coordinates(x)
+  }
+  
+  # The user can provide a custom vector to be plotted
+  # Otherwise, plot variogram of residuals
+  if(missing(z)) {
+    z <- residuals(x)
+  }
   
   # Check: Only one observation per spatial unit
   if( any(duplicated(coord)) )
@@ -40,7 +78,7 @@ variogram <- function(x, R, plot = c('all', 'isotropic', 'anisotropic', 'perspec
   # Matrix of residuals by rows and cols
   dat <- sparseMatrix(i = as.integer(coord1[[1]]),
                       j = as.integer(coord1[[2]]),
-                      x = resid
+                      x = z
                       , dimnames = lapply(coord1, levels)
                       )
   
@@ -89,7 +127,21 @@ variogram <- function(x, R, plot = c('all', 'isotropic', 'anisotropic', 'perspec
                                breedR.getOption('col.seq')[2]) )
   nbcol = 100
   colours <- col.f(nbcol)
-  facetcol <- cut(dat.halfheat$z, nbcol)
+  
+  # Computes the facet's means without taking into account NAs
+  meanmat.f <- function(x) {
+    nx = nrow(x)
+    ny = ncol(x)
+    tmp <- cbind(as.vector(x[-1, -1]),
+                 as.vector(x[-1, -ny]),
+                 as.vector(x[-nx, -1]),
+                 as.vector(x[-nx, -ny]))
+    
+    mv <- apply(tmp, 1, mean, na.rm = TRUE)
+    return(matrix(mv, nrow = nx - 1))
+  }
+  
+  facetcol <- cut(meanmat.f(dat.mhalf), nbcol)
   
   # I can't store the plot itself in an object
   # but I can stor the arguments for persp() in a list
@@ -105,11 +157,28 @@ variogram <- function(x, R, plot = c('all', 'isotropic', 'anisotropic', 'perspec
                         ylab = 'col disp.',
                         zlab = '')
 
+  #   # DEBUG
+  #   # How colour assignement works
+  #   nx <- ny <- 4
+  #   tdat <- matrix(NA, nrow = nx, ncol = ny)
+  #   tz <- 1:(nx*(ny+1)/2)
+  #   tdat[c(1:7, 9:10, 13)] <- tz
+  #   tcol <- col.f(100)
+  #   # Compute the z-value at the facet centres
+  #   # This gives problems with NAs 
+  #   tzfacet <- tdat[-1, -1] + tdat[-1, -ny] + tdat[-nx, -1] + tdat[-nx, -ny]
+  #   # Better use the values at the vertex closest to the origin
+  #   tzfacet <- tdat[-nx, -ny]
+  #   # Performing the mean with matrices with NAs
+  #   tzfacet <- meanmat.f(tdat)
+  #   tfcol <- cut(tzfacet, 50)
+  #   persp(x = 1:4, y = 1:4, z = tdat, col = tcol[tfcol], phi = 30, theta = -35)
+  
   # Visualization
   
   #   # Residuals
   #   image(dat)
-  #   ggplot(data.frame(coord1, z = resid), aes(x, y, fill = z)) +
+  #   ggplot(data.frame(coord1, z = z), aes(x, y, fill = z)) +
   #     geom_raster() + 
   #     coord_fixed() +
   #     scale_fill_gradient(low='green', high='red')
