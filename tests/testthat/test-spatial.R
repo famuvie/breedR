b7025a3365ea334ff30ca80010d5018ea2ce89b7
slecourieux <slecourieux@gmail.com>
2015-05-05 16:04:10 +0200

    Update test-spatial
    tests of non-consecutive lags, non-integer coordinates and
    negative-valued coordinates

--- tests/testthat/test-spatial.R
+++ tests/testthat/test-spatial.R
@@ -13,12 +13,19 @@ tcl <- list(
   # replicates in same position
   reps = list(coord = data.frame(x = c(1:3, 3:6), y = c(0:4, 4:5)),
               expct = list(fill = list(x = 1:6, y = 0:5),
-                           nofill = list(x = 1:6, y = 0:5)))
-  
-  # TODO: 
-  #   - non-consecutive lags
-  #   - non-integer coordinates
-  #   - negative-valued coordinates
+                           nofill = list(x = 1:6, y = 0:5))),
+  # non-consecutive lags
+  nclags = list(coord = data.frame(x = c(1,3,5,7,11,13,15), y = c(2,4,8,10,12,14,16)),
+              expct = list(fill = list(x = c(1,3,5,7,9,11,13,15), y = c(2,4,6,8,10,12,14,16)),
+                           nofill = list(x = c(1,3,5,7,11,13,15), y = c(2,4,8,10,12,14,16)))),
+  # non-integer coordinates
+  nicoords = list(coord = data.frame(x = c(1.2,2.4,3.6,4.8,7.2), y = c(0.5,1,1.5,2.5,3)),
+              expct = list(fill = list(x = c(1.2,2.4,3.6,4.8,6,7.2), y = c(0.5,1,1.5,2,2.5,3)),
+                           nofill = list(x = c(1.2,2.4,3.6,4.8,7.2), y = c(0.5,1,1.5,2.5,3)))),
+  # negative-valued coordinates 
+  nvcoords = list(coord = data.frame( x= c(-9:-5,-3:1), y = c(-11:-9,-7:-1)),
+              expct = list(fill = list(x = c(-9:1), y = c(-11:-1)),
+                           nofill = list(x = c(-9:-5,-3:1), y = c(-11:-9,-7:-1))))
   )
 
 ## Debug: plot datasets
@@ -35,5 +42,3 @@ test_that("loc_grid behaves as expected", {
   }
 })
 
-## TODO:
-##   - test functions fill_holes() and fill_hole() (see defs in ../R/spatial.R)
