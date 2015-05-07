911e17b134a99a9e2120a1d5f3c9a07107a57f92
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-05-07 13:01:19 +0200

    new function build_grid() to build a minimal encompassing spatial grid

--- R/spatial.R
+++ R/spatial.R
@@ -109,3 +109,57 @@ fill_hole <- function(x, idx, n, sep, label) {
                'as needed and use autofill = FALSE\n'))
   return(c(head(x, idx), filling, tail(x, -idx)))
 }
+
+
+#' Build an encompassing grid
+#' 
+#' Build the minimal regularly-spaced grid containing a given set of points.
+#' 
+#' @param coordinates two-column matrix-like set of row and column coordinates 
+#'   of observational units
+#' @param autofill logical. If TRUE it will try to fill missing rows or columns 
+#'   with missing observations. Otherwise, will treat individuals as neighbours 
+#'   even if they are across an empty line.
+#'   
+#' @return The parameters defining the grid, and the index of the observed 
+#'   coordinates in the grid seen as a vector. More specifically, \describe{ 
+#'   \item{origin}{the coordinates of the \emph{first} (with smallest row and
+#'   column values) point in the grid} \item{step}{the separation between rows
+#'   and columns} \item{length}{the number of points in each dimension} 
+#'   \item{idx}{the index of each observation in the vectorized grid} }
+build_grid <- function (coordinates, autofill = TRUE) {
+  
+  stopifnot(ncol(coord) == 2)
+  
+  ## Original coordinates
+  coord0 <- as.data.frame(coordinates)
+  
+  ## spatial locations by rows and columns
+  ## possibly with automatic filling of empty rows or columns
+  pos <- loc_grid(coord0, autofill)
+  
+  # The coordinates as factors allows to find easily the number of 
+  # different x and y positions, and the ordering
+  coord <- as.data.frame(mapply(factor,
+                                coord0,
+                                pos,
+                                SIMPLIFY = FALSE))
+  
+  # Number of different locations in rows and cols
+  pos.length <- vapply(pos, length, 1)
+  
+  # Spacing between trees in rows and columns
+  pos.step <- vapply(pos, function(x) diff(x)[1], 1)
+  
+  # Map data coordinates with corresponding index of the Q matrix
+  matrix2vec <- function(x, nx = pos.length[1], ny = pos.length[2]) {
+    map <- matrix(1:(nx*ny), nx, ny)
+    return(apply(x, 1, function(y) map[y[1], y[2]]))
+  }
+  ord <- matrix2vec(sapply(coord, as.integer))
+  
+  return(list(origin   = vapply(pos, head, 1, 1),
+              step     = pos.step,
+              length   = pos.length,
+              idx      = ord))
+}
