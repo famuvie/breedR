47277183fb532a381382d830f2d53ccda7619243
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-05-07 15:46:39 +0200

    make incidence and structure matrices sparse in the random() constructor rather than in generic
    its a more general operation that can be used by other random effects as well

--- R/generic.R
+++ R/generic.R
@@ -13,25 +13,11 @@ generic <- function(incidence, covariance, precision) {
   
   ## Try converting to Matrix class (either sparse or dense)
   mc <- match.call()
-  arg.list <- as.list(mc)[-1]
-  as.Matrix <- function(x) {
-    ## If it is not already a Matrix, try conversion to a regular matrix
-    ## and then to a Matrix class
-    if (!inherits(x, 'Matrix')) {
-      x <- as(as.matrix(x), 'Matrix')
-    }
-    return(x)
-  }
-  for (arg in names(arg.list)) {
-    mc[[arg]] <- try(as.Matrix(eval(mc[[arg]], parent.frame())))
-    if (inherits(mc[[arg]], 'try-error'))
-      stop(paste(arg, 'is expected to be a matrix-like object in a generic model.'))
-  }
 
   ## Build the random effect, and further specify the generic class
   random.call <- mc
   random.call[[1]] <- as.symbol('random')
-  ans <- eval(random.call)
+  ans <- eval(random.call, parent.frame())
   class(ans) <- c('generic', class(ans))
   
   return(ans)
--- R/random.R
+++ R/random.R
@@ -17,26 +17,42 @@
 #'   \code{covariance} or \code{precision}.
 random <- function (incidence, covariance, precision) {
   
+  mc <- match.call()
+  arg.list <- as.list(mc)[-1]
+
+  as.Matrix <- function(x) {
+    ## If it is not already a Matrix, try conversion to a regular matrix
+    ## and then to a Matrix class
+    if (!inherits(x, 'Matrix')) {
+      x <- as(as.matrix(x), 'Matrix')
+    }
+    return(x)
+  }
+
   ## Checks ======================================================
   ## arguments
   if (!xor(missing(covariance), missing(precision)))
     stop('Exactly one of covariance and precision matrices
          must be specified in a random effect.')
 
+  for (arg in names(arg.list)) {
+    mc[[arg]] <- try(as.Matrix(eval(mc[[arg]], parent.frame())))
+    if (inherits(mc[[arg]], 'try-error'))
+      stop(paste(arg, 'is expected to be a matrix-like object in a random effects model.'))
+  }
+  
   ## Define structure either with covariance or precision
-  mc <- match.call()
-  arg.list <- as.list(mc)[-1]
   type      <- grep("covariance|precision",
                     names(arg.list),
                     value = TRUE)
-  structure <- eval(arg.list[[type]])
+  structure <- eval(mc[[type]], parent.frame())
   
   ## conformance
   if (ncol(incidence) != nrow(structure))
     stop('Non conforming incidence and structure matrices.')
 
   ## Build the random effect, and further specify the generic class
-  eff <- breedr_effect(incidence = incidence)
+  eff <- breedr_effect(incidence = mc[['incidence']])
   ans <- structure(c(eff,
                      list(structure.matrix = structure,
                           structure.type   = type)),
