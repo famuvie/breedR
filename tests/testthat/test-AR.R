355b90af015beed0523fe124895ede7ec10f4449
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-06-01 13:57:57 +0200

    update tests for spatial models
    check that parameters are returned in summary

--- tests/testthat/test-AR.R
+++ tests/testthat/test-AR.R
@@ -333,6 +333,7 @@ test_that("residuals() gets a vector of length N", {
 
 test_that("summary() shows summary information", {
   expect_output(summary(res), 'Variance components')
+  expect_output(summary(res), 'rho:')
 })
 
 test_that("vcov() gets the covariance matrix of the spatial component of the observations", {
--- tests/testthat/test-splines.R
+++ tests/testthat/test-splines.R
@@ -164,6 +164,7 @@ test_that("residuals() gets a vector of length N", {
 
 test_that("summary() shows summary information", {
   expect_output(summary(res), 'Variance components')
+  expect_output(summary(res), 'knots:')
 })
 
 test_that("vcov() gets the covariance matrix of the spatial component of the observations", {
