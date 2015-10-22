94e2d698324ba147e18527cfcb18d38d9a4e80bb
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-10-22 17:37:28 +0200

    install progsf90 at package-install time

--- src/install.libs.R
+++ src/install.libs.R
@@ -1,32 +1,15 @@
+# Script which will be run as part of the installation used to automate install 
+# of PROGSF90 binaries The script is run in a separate R environment containing 
+# the following variables: R_PACKAGE_NAME (the name of the package), 
+# R_PACKAGE_SOURCE (the path to the source directory of the package), 
+# R_PACKAGE_DIR (the path of the target installation directory of the package), 
+# R_ARCH (the arch-dependent part of the path, often empty), SHLIB_EXT (the
+# extension of shared objects) and WINDOWS (TRUE on Windows, FALSE elsewhere). 
+# REF:
+# http://cran.univ-paris1.fr/doc/manuals/r-release/R-exts.html#Package-subdirectories
 
-source_base <- "https://raw.githubusercontent.com/famuvie/breedR/master/inst/bin"
-execs <- c("remlf90", "airemlf90")
 
-if (WINDOWS) {
-  source_platform <- "windows"
-  execs <- paste0(execs, ".exe")
-}
+source('../R/binaries.R')
+source('../R/os.R')
 
-if (.Platform$OS.type == "unix") {
-  if (is.na((file.info("/Library")$isdir && file.info("/Applications")$isdir))) {
-    source_platform <- "linux"
-  } else {
-    source_platform <- "mac"
-  }
-}
-
-f.url <- file.path(source_base, source_platform, execs)
-
-## Download files, set execution permissions and move into place
-td <- tempdir()
-wd <- setwd(td)
-on.exit(setwd(wd))
-download.file(f.url, destfile = execs, method = 'libcurl', mode = 'wb')
-Sys.chmod(execs, mode = '0774')
-
-if ( any(file.exists(execs)) ) {
-  dest <- file.path(R_PACKAGE_DIR,  paste0('bin', R_ARCH))
-  dir.create(dest, recursive = TRUE, showWarnings = FALSE)
-  file.copy(execs, dest, overwrite = TRUE)
-}
-
+install_progsf90(dest = file.path(R_PACKAGE_DIR, 'bin'))
