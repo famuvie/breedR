00852295ba841d3d4c04cba2352814c056cd26b4
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-11-19 16:58:53 +0100

    Default heritability computation

--- tests/testthat/test-pf90_options.R
+++ tests/testthat/test-pf90_options.R
@@ -6,12 +6,11 @@ on.exit(options(old.op))
 
 context("PROGSF90 options")
 
-test_that('remlf90 passes progsf90.options correctly', {
+test_that('remlf90 parses progsf90.options correctly', {
 
   N = 100
   dat <- transform(data.frame(x = runif(N)),
                     y = 1 + 2*x + rnorm(N))
-  mf <- build.mf()
   
   expect_opt <- function(opt, regexp) {
     res <- suppressMessages(
@@ -54,3 +53,37 @@ test_that('remlf90 passes progsf90.options correctly', {
   expect_identical(length(grep('OPTION', parameters_file)), length(opts))
 })
 
+
+test_that('remlf90(method = "ai") returns heritability', {
+  
+  ## Simulate a dataset with a heritability of 2/(1+2+1+1) = 0.4
+  dat <- breedR.sample.phenotype(fixed   = c(mu = 10, x = 2),
+                                 random = list(u = list(nlevels = 3,
+                                                        sigma2  = 1)),
+                                 genetic = list(model    = 'add_animal',
+                                                Nparents = c(10, 10),
+                                                sigma2_a = 2,
+                                                check.factorial = FALSE),
+                                 spatial = list(model     = 'AR',
+                                                grid.size = c(15, 15),
+                                                rho       = c(.7, .8),
+                                                sigma2_s  = 1),
+                                 residual.variance = 1)
+  
+  res <- remlf90(phenotype ~ 1 + X.x,
+                 random = ~ u,
+                 genetic = list(model = 'add_animal',
+                                pedigree = dat[, 1:3],
+                                id = 'self'),
+                 spatial = list(model = 'AR',
+                                coord = dat[, c('Var1', 'Var2')],
+                                rho   = c(.7, .8)),
+                 data   = dat)
+
+  expect_true(
+    all(
+      any(grepl('* SE for function of \\(co\\)variances H2', res$reml$output)),
+      any(grepl('H2  - Function: ', res$reml$output))
+    )
+  )
+})
