c0f665a783f98cf94344730312748f1916cb88c1
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2015-11-19 10:47:33 +0100

    test-competition fix
    - using :: for an internal function broke tests on a foreign machine (but not in the developing macine, ¿?). Removed that. Not only was unnecessary but mistaken.
    
    - Improved test style using more clever use of expect_error.

--- tests/testthat/test-competition.R
+++ tests/testthat/test-competition.R
@@ -20,26 +20,26 @@ var.ini.mat <- matrix(c(1, -.5, -.5, 1), 2, 2)
 
 test_that("Valid alternative model specifications pass checks", {
   ## specify var.ini, but use pec=FALSE
-  comp.spec <- try(
-    breedR::check_genetic(model = 'competition',
-                          pedigree = ped,
-                          coordinates = dat[, c('x', 'y')],
-                          id = dat$id,
-                          var.ini = var.ini)
+  expect_error(
+    check_genetic(model = 'competition',
+                  pedigree = ped,
+                  coordinates = dat[, c('x', 'y')],
+                  id = dat$id,
+                  var.ini = var.ini),
+    NA
   )
-  expect_true(!inherits(comp.spec, 'try-error'))
   
   ## non-recoded pedigree
   idx <- attr(ped, 'map')[dat$id]
   dat[, 1:3] <- as.data.frame(ped)[idx, ]
-  comp.spec <- try(
-    breedR::check_genetic(model = 'competition',
-                          pedigree = dat[, 1:3],
-                          coordinates = dat[, c('x', 'y')],
-                          id = dat$id,
-                          var.ini = var.ini)
+  expect_error(
+    check_genetic(model = 'competition',
+                  pedigree = dat[, 1:3],
+                  coordinates = dat[, c('x', 'y')],
+                  id = dat$id,
+                  var.ini = var.ini),
+    NA
   )
-  expect_true(!inherits(comp.spec, 'try-error'))
   
 })
 
@@ -47,59 +47,55 @@ test_that("Valid alternative model specifications pass checks", {
 
 test_that("Invalid alternative model specifications fail checks", {
   ## specify var.ini, but use pec=FALSE
-  comp.spec <- try(
-    breedR::check_genetic(model = 'competition',
-                          pedigree = ped,
-                          coordinates = dat[, c('x', 'y')],
-                          id = dat$id,
-                          var.ini = var.ini)
+  expect_error(
+    check_genetic(model = 'competition',
+                  pedigree = ped,
+                  coordinates = dat[, c('x', 'y')],
+                  id = dat$id,
+                  var.ini = var.ini),
+    NA
   )
-  expect_true(!inherits(comp.spec, 'try-error'))
   
   ## incomplete non-recoded pedigrees
   idx <- attr(ped, 'map')[dat$id]
   dat[, 1:3] <- as.data.frame(ped)[idx, ]
-  comp.spec <- try(
-    breedR::check_genetic(model = 'competition',
-                          pedigree = dat[-nrow(dat), 1:3],
-                          coordinates = dat[, c('x', 'y')],
-                          id = dat$id,
-                          var.ini = var.ini),
+  expect_error(
+    check_genetic(model = 'competition',
+                  pedigree = dat[-nrow(dat), 1:3],
+                  coordinates = dat[, c('x', 'y')],
+                  id = dat$id,
+                  var.ini = var.ini), 
+    'The following individuals in id are not represented'
   )
-  expect_error(comp.spec, 
-               'The following individuals in id are not represented')
-  
 })
 
-## Full specification, from minimal input
-comp.spec <- check_genetic(model = 'competition',
-                           pedigree = ped,
-                           id = dat$id,
-                           coordinates = dat[, c('x', 'y')],
-                           pec = TRUE)
 
-res <- try(with(comp.spec,
-                additive_genetic_competition(pedigree    = pedigree,
-                                             coordinates = coordinates,
-                                             id          = id,
-                                             decay       = competition_decay,
-                                             autofill    = autofill)))
 
 test_that("additive_genetic_competition() works as expected", {
-  expect_false(inherits(res, 'try-error'))
+  ## Full specification, from minimal input
+  comp.spec <- check_genetic(model = 'competition',
+                             pedigree = ped,
+                             id = dat$id,
+                             coordinates = dat[, c('x', 'y')],
+                             pec = TRUE)
   
-  if (!inherits(res, 'try-error')) {
-    expect_is(res, c("additive_genetic_competition", "additive_genetic", "genetic", 
+  res <- with(comp.spec,
+              additive_genetic_competition(pedigree    = pedigree,
+                                           coordinates = coordinates,
+                                           id          = id,
+                                           decay       = competition_decay,
+                                           autofill    = autofill))
+  
+  expect_is(res, c("additive_genetic_competition", "additive_genetic", "genetic", 
                    "competition", "spatial", "random", "breedr_effect"))
-    expect_equal(length(res), 5)
-    # Incidence matrix
-    inc.mat <- model.matrix(res)
-    expect_is(inc.mat, 'sparseMatrix') # a permutation Matrix
-    expect_equal(nrow(inc.mat), nrow(dat))
-    # Covariance matrix
-    cov.mat <- get_structure(res)
-    expect_is(cov.mat, 'sparseMatrix') 
-    expect_equal(ncol(cov.mat), nrow(as.data.frame(ped)))
-    expect_equal(ncol(inc.mat), nrow(cov.mat))
-  }
+  expect_equal(length(res), 5)
+  # Incidence matrix
+  inc.mat <- model.matrix(res)
+  expect_is(inc.mat, 'sparseMatrix') # a permutation Matrix
+  expect_equal(nrow(inc.mat), nrow(dat))
+  # Covariance matrix
+  cov.mat <- get_structure(res)
+  expect_is(cov.mat, 'sparseMatrix') 
+  expect_equal(ncol(cov.mat), nrow(as.data.frame(ped)))
+  expect_equal(ncol(inc.mat), nrow(cov.mat))
 })
