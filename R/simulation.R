81009ada771f159c1d40482024e7b89a3e147594
Facundo Muñoz <facundo.munoz@orleans.inra.fr>
2016-05-10 15:22:32 +0200

    Partially undoing last bugfix
    I don't know what I wanted to solve, since I didn't link it with any issue or commited a new test. But it didn't work for the case in the example('simulation')

--- R/simulation.R
+++ R/simulation.R
@@ -193,7 +193,7 @@ breedR.sample.phenotype <- function(fixed = NULL,
     if( exists('map', attributes(ped)) ) {
       if ( !cf ) {
         rm.idx <- which(is.na(attr(ped, 'map')))
-        components <- lapply(components, function(x) x[-rm.idx,,drop=FALSE])
+        components <- components[-rm.idx,,drop=FALSE]
         phenotype  <- phenotype[-rm.idx]
         Nfull <- nrow(components)
       } else {
