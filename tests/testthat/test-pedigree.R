a20f413e84727bcad958a17b9e97eaa9c9048fff
Facundo Muñoz <facundo.munoz@cirad.fr>
2020-06-10 09:19:30 +0200

    test-pedigree: update expect_true() from {testthat}.

--- tests/testthat/test-pedigree.R
+++ tests/testthat/test-pedigree.R
@@ -15,8 +15,8 @@ data(m4)
 ped <- as.data.frame(m4)[, c('self', 'dad', 'mum')]
 
 test_that('The pedigree from m4 is not complete, but otherwise correct', {
-  expect_that(!check_pedigree(ped)['full_ped'], is_true());
-  expect_that(all(check_pedigree(ped)[-1]), is_true())
+  expect_true(!check_pedigree(ped)['full_ped'])
+  expect_true(all(check_pedigree(ped)[-1]))
 })
 
 # Generate a crazy map
@@ -31,12 +31,12 @@ ped_shuffled <- sapply(ped, function(x) map[x])
 ped_shuffled[, 2:3][sample(2*nrow(ped), 200)] <- c(0, NA)
 
 test_that('The shuffled pedigree fails all checks', {
-  expect_that(all(!check_pedigree(ped_shuffled)), is_true())
+  expect_true(all(!check_pedigree(ped_shuffled)))
 })
 
 # Reorder and recode 
 ped_fix <- suppressWarnings(build_pedigree(1:3, data = ped_shuffled))
 test_that('build_pedigree() fixes everything', {
-  expect_that(all(check_pedigree(ped_fix)), is_true())
+  expect_true(all(check_pedigree(ped_fix)))
 })
 
